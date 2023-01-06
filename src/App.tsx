2413f5599c83690b1c4036e10db53f6f7cf80b66
Grigory <grishka5556@gmail.com>
2023-01-06 21:14:45 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes} from "react-router-dom";
+import {ProductsPage} from "./pages/ProductsPage";
+import {AboutPage} from "./pages/AboutPage";
+import {Navigation} from "./components/Navigation";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navigation/>
+      <Routes>
+        <Route path='/' element={<ProductsPage/>}/>
+        <Route path='/about' element={<AboutPage/>}/>
+      </Routes>
+    </>
+  )
 }
 
 export default App;
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export function AboutPage() {
+  return (
+    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad animi culpa enim ex hic odio pariatur sunt vel voluptas. Ad architecto dignissimos facilis laborum possimus, quasi reiciendis temporibus veritatis vitae. A amet consectetur deserunt dignissimos doloremque et excepturi expedita fuga hic id officiis quaerat, recusandae sed tempore tenetur unde.</div>
+  );
+}
+
