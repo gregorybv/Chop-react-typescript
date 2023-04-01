43e9ec6cdf15c071d1316e5a5a64ac24f9a56e76
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-01 09:19:13 +0300

    new edits

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 export function AboutPage() {
   return (
-    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad animi culpa enim ex hic odio pariatur sunt vel voluptas. Ad architecto dignissimos facilis laborum possimus, quasi reiciendis temporibus veritatis vitae. A amet consectetur deserunt dignissimos doloremque et excepturi expedita fuga hic id officiis quaerat, recusandae sed tempore tenetur unde.</div>
+    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad animi culpa enim 
+      ex hic odio pariatur sunt vel voluptas. Ad architecto dignissimos facilis laborum possimus, 
+      quasi reiciendis temporibus veritatis vitae. A amet consectetur deserunt dignissimos doloremque
+       et excepturi expedita fuga hic id officiis quaerat, recusandae sed tempore tenetur unde.
+    </div>
   );
 }
 
