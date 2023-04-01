80e315946b1fde207290b4c7069c8daec5bbde13
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-01 09:19:07 +0300

    new edits

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,7 +5,6 @@ export function Navigation() {
   return (
     <nav className='h-[50px] flex justify-between px-5 bg-gray-500 items-center text-white'>
       <span className='font-bold'>React</span>
-
       <span>
         <Link to="/" className='mr-2'>Products</Link>
         <Link to="/about">About</Link>
